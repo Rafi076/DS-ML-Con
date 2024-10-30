ca8591c91a105026fae21e49310e8e9a62dc7ddb
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-30 23:43:11 +0600

    M3 test

---
+++ 1_Python Fun (Mod 1-3)/M3 QUE/liveTest.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+    def display(self):
+        print(f"Name: {self.name}, Roll No: {self.roll_no}")
+
+
+student = Student("John", 2)
+student.display()
